90e931a3380efecda3a087a17f1f6ab7c66fcce0
MikeyOnyedika <michaelvik520@gmail.com>
2024-10-05 13:21:51 +0100

    tablet view for technology page

--- src/app/(other)/technology/[[...slug]]/page.tsx
+++ src/app/(other)/technology/[[...slug]]/page.tsx
@@ -6,23 +6,15 @@ import { bellefair } from "@/app/font";
 
 function findTechnologyAndStyle(
   params: Record<string, string>
-): [(typeof technology)[0], string] | false {
+): (typeof technology)[0] | false {
   const slug = params.slug;
 
   if (!slug || slug[0] === "launch-vehicle") {
-    return [
-      technology.find((tech) => tech.slug === "launch-vehicle")!,
-      "object-cover -translate-x-[50%] -translate-y-[64%]"]
+    return technology.find((tech) => tech.slug === "launch-vehicle")!
   } else if (slug[0] === "spaceport") {
-    return [
-      technology.find((tech) => tech.slug === "spaceport")!,
-      "object-cover -translate-x-[50%] -translate-y-[55%] "
-    ]
+    return technology.find((tech) => tech.slug === "spaceport")!
   } else if (slug[0] === "space-capsule") {
-    return [
-      technology.find((tech) => tech.slug === "space-capsule")!,
-      "object-cover -translate-x-[50%] -translate-y-[55%]"
-    ]
+    return technology.find((tech) => tech.slug === "space-capsule")!
   } else {
     return false;
   }
@@ -33,31 +25,35 @@ export default function Technology({
 }: {
   params: Record<string, string>;
 }) {
-  const item = findTechnologyAndStyle(params);
-  if (item === false) {
+  const tech = findTechnologyAndStyle(params);
+  if (tech === false) {
     redirect("/technology");
   }
-  const [tech, imageStyle] = item;
   return (
-    <section className="w-full h-fit flex flex-col gap-8 pb-6">
-      <section className="pt-16 h-full">
-        <picture>
-          <source srcSet={tech.image2} media="(max-width: 767.9px)" />
-          <source srcSet={tech.image1} media="(min-width: 768px)" />
-          <div className="w-full h-[16rem] relative overflow-hidden">
-            <Image src={tech.image1} width={1000} height={1000} alt="" className={`w-full h-fit  absolute top-[50%] left-[50%] ${imageStyle}`} />
-          </div>
-        </picture>
+    <section className="w-full h-fit flex flex-col items-center  gap-8 pb-6 md:pb-10">
+      <section className="pt-16 h-full w-full">
+        <div className="flex justify-center w-full h-[258px] md:h-[356px] md:max-w-[768px] md:m-auto relative overflow-hidden">
+          <picture>
+            {/* image3 is for desktop view */}
+            <source srcSet={tech.image3} media="(min-width: 1440px)" />
+            {/* image2 is for tablet view */}
+            <source srcSet={tech.image2} media="(min-width: 768px)" />
+            {/*use the mobile version as the default (fallback) image*/}
+            <Image src={tech.image1} width={2000} height={2000} alt="" className={`w-full  h-full object-cover `} />
+          </picture>
+        </div>
       </section>
-      <section className="flex flex-col gap-10 px-6 pb-6">
-        <Nav selectedTechSlug={tech.slug} />
-        <section className="flex flex-col gap-4">
-          <div className="flex flex-col-reverse items-center gap-4 uppercase">
-            <h1 className={`text-2xl text-white ${bellefair.className}`}>{tech.name}</h1>
-            <h2 className={`text-lg text-white/50 ${bellefair.className}`}>The terminology...</h2>
-          </div>
-          <p className="text-[0.94rem] text-center leading-[180%]">{tech.description}</p>
-        </section>
+      <section className="flex justify-center w-full px-6 pb-6 md:px-10">
+        <div className="flex flex-col gap-10 w-full max-w-[32rem]">
+          <Nav selectedTechSlug={tech.slug} />
+          <section className="flex flex-col gap-4">
+            <div className="flex flex-col-reverse items-center gap-4 uppercase">
+              <h1 className={`text-2xl md:text-[2.5rem] text-white ${bellefair.className}`}>{tech.name}</h1>
+              <h2 className={`text-lg md:text-2xl text-white/50 ${bellefair.className}`}>The terminology...</h2>
+            </div>
+            <p className="text-[0.94rem] md:text-base md:leading-[180%] text-center leading-[180%]">{tech.description}</p>
+          </section>
+        </div>
       </section>
     </section>
   );
--- src/app/(other)/technology/components/Nav.tsx
+++ src/app/(other)/technology/components/Nav.tsx
@@ -8,7 +8,7 @@ export default function Nav({ selectedTechSlug }: { selectedTechSlug: string }) 
       <ul className="flex w-full justify-center gap-4" >
         {technologies.map((tech, index) => (
           <li key={tech.slug} className={`flex`}>
-            <Link href={tech.href} className={`w-10 h-10 rounded-full border-[1px] hover:border-white transition-all  text-lg ${bellefair.className} flex justify-center items-center ${selectedTechSlug === tech.slug ? "bg-white text-blue-900" : "border-white/25 bg-[transparent]"}`}>{index + 1}</Link>
+            <Link href={tech.href} className={`w-10 h-10 md:w-14 md:h-14 rounded-full border-[1px] hover:border-white transition-all  text-lg md:text-2xl ${bellefair.className} flex justify-center items-center ${selectedTechSlug === tech.slug ? "bg-white text-blue-900" : "border-white/25 bg-[transparent]"}`}>{index + 1}</Link>
           </li>
         ))}
       </ul>
