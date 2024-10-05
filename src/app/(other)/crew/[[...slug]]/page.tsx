b4eddb496176ec4305fa0d7ee49bccf4ed7a1431
MikeyOnyedika <michaelvik520@gmail.com>
2024-10-05 22:22:25 +0100

    desktop view for crew page

--- src/app/(other)/crew/[[...slug]]/page.tsx
+++ src/app/(other)/crew/[[...slug]]/page.tsx
@@ -33,19 +33,19 @@ export default function Crew({
   }
 
   return (
-    <section className="w-full h-full flex flex-col items-center gap-8 justify-between pb-6 md:pb-10 pr-6 md:pr-10 pl-6 md:pl-10">
-      <section className="w-full flex flex-col gap-6 pt-10 max-w-[32rem]">
-        <section className="flex flex-col gap-6">
-          <div className="flex flex-col-reverse items-center gap-2 md:gap-4 uppercase">
-            <h1 className={`text-2xl md:text-[2.5rem] text-white ${bellefair.className}`}>{crewMember.name}</h1>
-            <h2 className={`text-lg md:text-2xl text-white/50 ${bellefair.className}`}>{crewMember.role}</h2>
+    <section className="w-full h-full flex flex-col items-center gap-8 justify-between pb-6 md:pb-10 pr-6 md:pr-10 pl-6 md:pl-10 2xl:flex-row">
+      <section className="w-full flex flex-col gap-6 pt-10 2xl:pt-0 max-w-[32rem] 2xl:max-w-[33.69rem] 2xl:gap-10 2xl:h-full">
+        <section className="flex flex-col gap-6 2xl:h-full 2xl:items-center 2xl:justify-center">
+          <div className="flex flex-col-reverse items-center gap-2 md:gap-4 uppercase 2xl:items-start 2xl:w-full">
+            <h1 className={`text-2xl md:text-[2.5rem] text-white 2xl:w-full 2xl:text-[3.5rem] 2xl:leading-[1.5] ${bellefair.className}`}>{crewMember.name}</h1>
+            <h2 className={`text-lg md:text-2xl 2xl:text-[2rem]  text-white/50 ${bellefair.className}`}>{crewMember.role}</h2>
           </div>
-          <p className="text-center text-[0.94rem] leading-[180%] min-h-[10.8em] md:text-base   md:leading-[180%] md:min-h-[7.2em] ">{crewMember.bio}</p>
+          <p className="text-center text-[0.94rem] leading-[180%] min-h-[10.8em] md:text-base   md:leading-[180%] md:min-h-[7.2em] 2xl:text-start 2xl:text-lg 2xl:leading-[180%]">{crewMember.bio}</p>
         </section>
         <Nav selectedCrewSlug={crewMember.slug} />
       </section>
-      <section className="w-full flex justify-center items-center h-full p-1">
-        <Image src={crewMember.image} width={1600} height={2400} alt="" className="w-[17rem] h-[17rem] md:w-[28.9rem] md:h-[35rem] object-contain" />
+      <section className="w-full flex justify-center items-center h-full p-1 ">
+        <Image src={crewMember.image} width={1600} height={2400} alt="" className="w-[17rem] h-[17rem] md:w-[28.9rem] md:h-[35rem] 2xl:w-[33.8rem] 2xl:h-auto object-contain 2xl:ps-8" />
       </section>
     </section>
   );
--- src/app/(other)/crew/components/Nav.tsx
+++ src/app/(other)/crew/components/Nav.tsx
@@ -3,11 +3,11 @@ import { crew } from "@/app/data";
 
 export default function Nav({ selectedCrewSlug }: { selectedCrewSlug: string }) {
   return (
-    <nav className="w-full flex justify-center">
-      <ul className="flex items-center gap-4">
+    <nav className="w-full flex justify-center 2xl:justify-start 2xl:pb-12">
+      <ul className="flex items-center gap-4 2xl:gap-10">
         {crew.map((crewMember) => (
           <li key={crewMember.slug} className="flex">
-            <Link href={crewMember.href} className={`w-[0.63rem] h-[0.63rem] transition-colors hover:bg-white/50 rounded-full ${selectedCrewSlug === crewMember.slug ? "bg-white" : "bg-white/20"}`}>
+            <Link href={crewMember.href} className={`w-[0.63rem] h-[0.63rem] 2xl:w-[0.95rem] 2xl:h-[0.95rem] transition-colors hover:bg-white/50 rounded-full ${selectedCrewSlug === crewMember.slug ? "bg-white" : "bg-white/20"}`}>
             </Link>
           </li>
         ))}
