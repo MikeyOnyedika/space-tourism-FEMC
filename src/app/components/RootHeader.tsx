566cad4307ca5635f82ce241dcdddf5718a99600
MikeyOnyedika <michaelvik520@gmail.com>
2024-10-05 14:10:21 +0100

    desktop view for root header

--- src/app/components/RootHeader.tsx
+++ src/app/components/RootHeader.tsx
@@ -14,19 +14,19 @@ export default function RootHeader() {
     redirect("/")
   }
   return (
-    <header className="flex justify-between items-center gap-2 p-6 md:p-0 md:gap-0 w-full">
-      <div className="flex items-center md:px-10 md:py-6">
-        <Image src="/assets/shared/logo.svg" alt="logo" width={40} height={40} className="w-10 h-10 md:min-w-12 md:w-12 md:h-12" />
+    <header className="flex justify-between items-center gap-2 p-6 md:p-0 md:gap-0 w-full 2xl:pt-10">
+      <div className="w-fit 2xl:w-full 2xl:flex 2xl:items-center 2xl:gap-[43px]">
+        <div className="flex items-center md:px-10 md:py-6 2xl:ps-16 2xl:pe-0 ">
+          <Image src="/assets/shared/logo.svg" alt="logo" width={40} height={40} className="w-10 h-10 md:min-w-12 md:w-12 md:h-12" />
+        </div>
+        <div className="hidden 2xl:block h-[1px] bg-white/25 w-full translate-x-[21px] z-10"></div>
       </div>
-      <button className="md:hidden" type="button" onClick={() => {
-        console.log("open?: ", globalState?.isNavOpen);
-        globalState?.openNav()
-      }}>
+      <button className="md:hidden" type="button" onClick={() => globalState?.openNav()}>
         <Image src="/assets/shared/icon-hamburger.svg" alt="hamburger-icon" width={24} height={24} className="w-6 aspect-auto" />
       </button>
       {/*Nav on Tablet and Laptop screens*/}
       <nav className={`hidden md:flex items-center  w-full backdrop-blur-xl bg-white/5   h-full`}>
-        <ul className="flex gap-12 px-10 w-full justify-end  h-full">
+        <ul className="flex gap-12 px-10 2xl:px-16 w-full justify-end  h-full">
           {navLinks.map(navItem => {
             if (navItem.slug === currentPage.slug) { console.log(navItem); }
             return (
